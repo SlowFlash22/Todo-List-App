50a024a263081dd5437c6650e11cfb211ff4f419
Prajjwal Mathur <64896645+SlowFlash22@users.noreply.github.com>
2022-10-28 14:26:15 +0530

    Updates

---
+++ precache-manifest.c91646150c1043ff2ac2d27be1d36064.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b10ff237ad7c5944ab3",
+    "url": "/Todo-List-App/static/css/main.a48bf073.chunk.css"
+  },
+  {
+    "revision": "0b10ff237ad7c5944ab3",
+    "url": "/Todo-List-App/static/js/main.0b10ff23.chunk.js"
+  },
+  {
+    "revision": "0765819dcae452713017",
+    "url": "/Todo-List-App/static/js/1.0765819d.chunk.js"
+  },
+  {
+    "revision": "3b8d16029fbc990dc340",
+    "url": "/Todo-List-App/static/js/2.3b8d1602.chunk.js"
+  },
+  {
+    "revision": "1e7c4626f5146bfc1e04",
+    "url": "/Todo-List-App/static/js/runtime~main.1e7c4626.js"
+  },
+  {
+    "revision": "4a45479c3dbf1317e55cc79108eeeae3",
+    "url": "/Todo-List-App/index.html"
+  }
+];
